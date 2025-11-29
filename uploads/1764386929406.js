91473ae613be9d37e6402a9d85ba0997e83588b1
my-databasepixel <wuyunnsahputra@gmail.com>
2025-11-29 10:28:49 +0700

    Upload file 1764386929406.js

---
+++ uploads/1764386929406.js
@@ -0,0 +1,27 @@
+{
+  "telegram_token": "8545370979:AAFH2E9VNtksMsXnBfHvEsLfgX-MKXLX6Vc",
+  "admins": ["6208011594"],
+  "admin_username": "@anhmqi",
+  "bot_name": "🔥 xLqcya Auto-Orders",
+  "payment_methods": {
+    "qris": "https://files.catbox.moe/srmiux.json",
+    "dana": "081234567890",
+    "gopay": "081234567890",
+    "ovo": "081234567890",
+    "bri": "1234-5678-9012-3456"
+  },
+  "firebase": {
+    "apiKey": "AIzaSyBHlqtjkzJUoTcEMQ9rfhDqNmepwcmF5v4",
+    "authDomain": "auto-e075d.firebaseapp.com",
+    "projectId": "auto-e075d",
+    "storageBucket": "auto-e075d.firebasestorage.app",
+    "messagingSenderId": "154774543272",
+    "appId": "1:154774543272:web:37e3c6b191012e9ba8db9d"
+  },
+  "settings": {
+    "referral_bonus": 1000,
+    "min_withdraw": 5000,
+    "auto_send_product": true,
+    "maintenance_mode": false
+  }
+}
