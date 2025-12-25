e09685b1c14e2e70b0aa4d8b8ef404f1b56c08ba
my-databasepixel <wuyunnsahputra@gmail.com>
2025-12-25 12:46:35 +0700

    Upload file 1766641595199_asmaulhusna.js

---
+++ uploads/1766641595199_asmaulhusna.js
@@ -0,0 +1,85 @@
+⟪ Alicia Telebotz ⟫
+Selamat Malam 🌙, ZenzzXD!
+
+╭─❏ INFORMASI BOT
+│ Owner       : ZenzzXD
+│ Prefix      : /
+│ Limit Anda  : 25
+│ Uptime      : 0d 00h 00m 56s
+│ Total Plugin: 48
+│ Pengguna    : 8
+╰───────────────❏
+
+📁 Admin
+/del
+/setleft
+/group
+
+📁 Ai
+/perplexity Ⓛ
+
+📥 Downloader
+/spotify
+/fbdl
+/instagramdl
+/pinterestdl
+/play
+/threadsdl
+/tiktok Ⓟ Ⓛ
+/ytmp3
+/ytmp4
+
+🪩 Group
+/kick
+/setwelcome
+
+📡 Info
+/credits
+/stokgag Ⓛ
+
+🌐 Internet
+/anime Ⓛ
+/gempa Ⓛ
+/darkjoke Ⓛ
+/github Ⓛ
+/githubsearch
+/wiki Ⓛ
+
+📄 Main
+/afk
+/allmenu
+/stats
+/menu
+
+📁 Maker
+/carbon
+/commentig
+/fstory
+/ytcomment
+
+👑 Owner
+/backupsc
+/ban
+/botmode
+/delplugins
+/getplugins
+/leavegroup
+/setlimit
+/listgroups
+/addowner
+/addprem
+/reload
+/restart
+/saveplugins
+
+📁 Sticker
+/brat Ⓛ
+/bratvid Ⓛ
+
+🛠️ Tools
+/hd Ⓟ Ⓛ
+/tourl Ⓛ
+
+Keterangan:  
+Ⓟ: Perintah Khusus Premium  
+Ⓛ: Perintah Menggunakan Limit
